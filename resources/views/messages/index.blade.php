4aec52f32421f73717fef7192ac7722e1e83af0f
Roxy Rodriguez-Becker <roxy.rodriguez@gmail.com>
2025-11-29 18:25:42 -0600

    removed RSS subscripe option

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -12,13 +12,13 @@
         <div class="text-center mb-12">
             <h1 class="text-4xl font-bold text-gray-900 mb-2">Tube Feed Tracker Updates</h1>
             <p class="text-gray-600">Latest features, tips, and messages</p>
-            <a href="{{ route('messages.rss') }}" class="inline-flex items-center mt-4 text-blue-600 hover:text-blue-700">
+            {{-- <a href="{{ route('messages.rss') }}" class="inline-flex items-center mt-4 text-blue-600 hover:text-blue-700">
                 <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M5 3a1 1 0 000 2c5.523 0 10 4.477 10 10a1 1 0 102 0C17 8.373 11.627 3 5 3z"/>
                     <path d="M4 9a1 1 0 011-1 7 7 0 017 7 1 1 0 11-2 0 5 5 0 00-5-5 1 1 0 01-1-1zM3 15a2 2 0 114 0 2 2 0 01-4 0z"/>
                 </svg>
                 Subscribe via RSS
-            </a>
+            </a> --}}
         </div>
 
         <!-- messages -->
