504cb19735d4b4bef18305a6f3acd06a61ca475d
Roxy Rodriguez-Becker <roxy.rodriguez@gmail.com>
2025-12-30 07:31:52 -0600

    eager loading

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -9,13 +9,22 @@ class MessageController extends Controller
 {
     public function index()
     {
-        $messages = Message::published()->get();
+        $messages = Message::with('user')
+            ->published()
+            ->latest()
+            ->paginate(20);
+
         return view('messages.index', compact('messages'));
     }
 
     public function rss()
     {
-        $messages = Message::published()->get();
+        $messages = Message::with('user')
+            ->published()
+            ->latest()
+            ->limit(50) 
+            ->get();
+            
         return response()
             ->view('messages.rss', compact('messages'))
             ->header('Content-Type', 'application/rss+xml; charset=utf-8');
